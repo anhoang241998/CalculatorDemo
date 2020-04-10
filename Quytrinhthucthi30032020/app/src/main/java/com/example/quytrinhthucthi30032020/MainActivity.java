8f6faf6675744e1cee0992337e79db6fd20b3c1c
HoangAn <an2419998@gmail.com>
2020-04-10 22:35:52 +0700

    add main file, demo gifs, and README.md

---
+++ Quytrinhthucthi30032020/app/src/main/java/com/example/quytrinhthucthi30032020/MainActivity.java
@@ -0,0 +1,100 @@
+package com.example.quytrinhthucthi30032020;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText mEditText_number1;
+    EditText mEditText_number2;
+    Button mButton_reset, mButton_plus, mButton_minus, mButton_multiply, mButton_divine;
+    TextView mResult_textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //khai báo biến
+        mEditText_number1 = findViewById(R.id.edit_number1);
+        mEditText_number2 = findViewById(R.id.edit_number2);
+        mButton_reset = findViewById(R.id.reset_button);
+        mButton_plus = findViewById(R.id.plus_button);
+        mButton_divine = findViewById(R.id.divine_button);
+        mButton_multiply = findViewById(R.id.multiply_button);
+        mButton_minus = findViewById(R.id.minus_button);
+        mResult_textView = findViewById(R.id.result_text);
+
+
+        //Plus Button
+        mButton_plus.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View v) {
+                String value1 = mEditText_number1.getText().toString();
+                String value2 = mEditText_number2.getText().toString();
+                int number1 = Integer.parseInt(value1);
+                int number2 = Integer.parseInt(value2);
+                int result = number1 + number2;
+                mResult_textView.setText(Integer.toString(result));
+            }
+        });
+
+        //Minus Button
+        mButton_minus.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View v) {
+                String value1 = mEditText_number1.getText().toString();
+                String value2 = mEditText_number2.getText().toString();
+                int number1 = Integer.parseInt(value1);
+                int number2 = Integer.parseInt(value2);
+                int result = number1 - number2;
+                mResult_textView.setText(Integer.toString(result));
+            }
+        });
+
+        //Multiply Button
+        mButton_multiply.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View v) {
+                String value1 = mEditText_number1.getText().toString();
+                String value2 = mEditText_number2.getText().toString();
+                int number1 = Integer.parseInt(value1);
+                int number2 = Integer.parseInt(value2);
+                int result = number1 * number2;
+                mResult_textView.setText(Integer.toString(result));
+            }
+        });
+
+        //Divine Button
+        mButton_divine.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View v) {
+                String value1 = mEditText_number1.getText().toString();
+                String value2 = mEditText_number2.getText().toString();
+                int number1 = Integer.parseInt(value1);
+                int number2 = Integer.parseInt(value2);
+                float result = number1 / (number2 * 1.0f);
+                mResult_textView.setText(Float.toString(result));
+            }
+        });
+
+        mButton_reset.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View v) {
+                mResult_textView.setText("");
+            }
+        });
+
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # CalculatorDemo
-Demo for calculator
+### Demo for calculator
+
+Đây là demo mình làm app calculator bài tập của lớp Android Khoa Phạm. Mình sẽ bổ sung lại phần giao diện sau. 
+
+![picture](https://media.giphy.com/media/ZdxA0kz8E5ExC5AalI/giphy.gif)
+
+### How to download / clone it
+
+Các bạn có thể download (clone) về bằng 2 cách:
+
+-   Cách 1: Clone về
+   -   Các bạn nhấn vào góc phải màn hình có nút `clone or download`. Các bạn clone về bằng cách sao chép cái đường link trong cái ô `clone with HTTPS`.
+   
+   -    Các bạn nhấp phải chuột tại folder muốn lưu file hoặc ra desktop, chọn `git bash here`
+   
+   -    Sau đó các bạn gõ câu lệnh: `git clone` ấn nút phải chuột paste link mới chép vô.
+   
+        Vd: `git clone https://github.com/anhoang241998/CalculatorDemo.git`
+        
+   - Vậy là xong.
+   
+- Cách 2: download về
+  
+   - Cách này đơn giản hơn, các bạn chỉ cần nhấn vào góc phải màn hình có nút `clone or download`. Các bạn chọn `Download ZIP`
+   
+### Tổng kết
+
+Đây chỉ là demo, code chuẩn sẽ được thầy dạy, nếu ai muốn tham khảo cách của mình thì xem nha.
+
+  ​     
+
